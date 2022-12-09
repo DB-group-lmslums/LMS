38ba450cbf5c88a567b5ae21cbfe80a9690a7a88
Shabbir-Hussain-7 <2019n00032@gmail.com>
2022-12-09 19:13:36 +0500

    cookie instead of session2

--- app.js
+++ app.js
@@ -340,8 +340,8 @@ app.post("/studenthome", async (req,res)=>{
     console.log(x);
     console.log(y);
     console.log(z);
-    console.log("req ies:", req.cookies.userinfo)
-    console.log("req ie:", req.cookie.userinfo)
+    // console.log("req ies:", req.cookies.userinfo)
+    // console.log("req ie:", req.cookie.userinfo)
     console.log("req sess:", req.session.userinfo)
     console.log("res:", res.cookies.userinfo)
     req.session.userinfo.courseID = x;
